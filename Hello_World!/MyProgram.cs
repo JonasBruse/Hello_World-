a01098dd45341c5e826e7b27a39159a4be4a6980
JonasBruse <jonas.andersson@elev.ga.lbs.se>
2017-01-18 16:36:51 +0100

    Created github repository

---
+++ Hello_World!/MyProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_
+{
+    class MyProgram
+    {
+        public void PrintMessage()
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message);
+        }
+    }
+}
